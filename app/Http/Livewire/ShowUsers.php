6f63ba3ef65c18f31c18db2a9e9cce489f77aac3
mario avila <avilamario16@gmail.com>
2024-05-20 00:55:27 -0600

    feat: toggle and search by name

--- app/Http/Livewire/ShowUsers.php
+++ app/Http/Livewire/ShowUsers.php
@@ -7,34 +7,48 @@ use App\Profile;
 
 class ShowUsers extends Component
 {
-
     public $profiles;
     public $name;
     public $last_name;
+    public $isCreating;
+    public $foundProfiles;
+    public $search_query;
+    public $render_profiles;
 
     function mount() {
-        $this->name = 'jons';
         $this-> fetchProfiles();
+        $this->isCreating = true;
+        $this->foundProfiles = Profile::where('id', 'LIKE', 0)->get();
     }
 
     function fetchProfiles() {
         $this-> profiles = Profile::all()->reverse();
+        $this-> render_profiles = $this-> profiles;
     }
 
-    function asdfu() {
-        $this->name = "yea";
-        // if($this->name != '' && last_name != '') {
-        //     // $profile = new Profile();
-        //     // $profile->name = $this->name;
-        //     // $profile->last_name = $this->last_name;
-        //     // $profile->save();
-        //     // $this->name = '';
-        //     // $this->last_name = '';
-        //     // $this->fetchProfiles();
-        // } else {
-        //     $this->name = "yea";
-        // }
+    function addProfile() {
+        if($this->name != '' && $this->last_name != '') {
+            $profile = new Profile();
+            $profile->name = $this->name;
+            $profile->last_name = $this->last_name;
+            $profile->save();
+            $this->name = '';
+            $this->last_name = '';
+            $this->fetchProfiles();
+        }
+    }
 
+    function searchUser() {
+        $this->foundProfiles = Profile::where('name', 'LIKE', '%'.$this->search_query.'%')->get();
+    }
+
+    function toggleIsCreating($value) {
+        $this->isCreating = $value;
+        if($value) {
+            $this->render_profiles = $this->profiles; 
+        } else {
+            $this->render_profiles = $this->foundProfiles;
+        }
     }
 
     public function render()
--- resources/views/livewire/show-users.blade.php
+++ resources/views/livewire/show-users.blade.php
@@ -1,31 +1,70 @@
 <div class="flex justify-center">
-    <div>
-        <input type="text" wire:model.live="name" placeholder="name" />
-        <input type="text" wire:model="last_name" placeholder="last name" />
-        <button wire:click="asdfu">submit new profile</button>
+    <button wire:click="toggleIsCreating(true)">Create new Users</button>
+    <button wire:click="toggleIsCreating(false)">Search users</button>
+    @if ($isCreating)
+        <h2>You are creating an user</h2>
+        <div>
+            <input type="text" wire:model.live="name" placeholder="name" />
+            <input type="text" wire:model="last_name" placeholder="last name" />
+            <button wire:click="addProfile">SUBMIT NEW PROFILE</button>
+        </div>
         <br>
-        <br>
-        <h1>asdfasdf {{$name}}<h1>
-        @if ($profiles->isEmpty())
-            <p>no profiles here</p>
-        @else
-            <table class="table-auto">
-                <th>
-                    <tr>
-                        <td>name</td>
-                        <td>last name</td>
-                        <td>actions</td>
-                    </tr>
-                </th>
-                <tbody>
-                    @foreach ($profiles as $profile)
+        <div>
+            <br>
+            <br>
+            @if ($profiles->isEmpty())
+                <p>no profiles here</p>
+            @else
+                <table class="table-auto">
+                    <th>
                         <tr>
-                            <td>{{ $profile->name }}</td>
-                            <td>{{ $profile->last_name }}</td>
+                            <td>name</td>
+                            <td>last name</td>
+                            <td>actions</td>
                         </tr>
-                    @endforeach
-                </tbody>
-            </table>
-        @endif
-    </div>
+                    </th>
+                    <tbody>
+                        @foreach ($profiles as $profile)
+                            <tr>
+                                <td>{{ $profile->name }}</td>
+                                <td>{{ $profile->last_name }}</td>
+                            </tr>
+                        @endforeach
+                    </tbody>
+                </table>
+            @endif
+        </div>
+    @else
+        <h2>You are searching for an user</h2>
+        <div>
+            <input type="text" wire:model.live="search_query" wire:keydown.enter placeholder="search for..." />
+            <button wire:click="searchUser">SEARCH</button>
+        </div>
+        <br>
+        <div>
+            <br>
+            <br>
+            @if ($foundProfiles->isEmpty())
+                <p>no profiles here</p>
+            @else
+                <table class="table-auto">
+                    <th>
+                        <tr>
+                            <td>name</td>
+                            <td>last name</td>
+                            <td>actions</td>
+                        </tr>
+                    </th>
+                    <tbody>
+                        @foreach ($foundProfiles as $profile)
+                            <tr>
+                                <td>{{ $profile->name }}</td>
+                                <td>{{ $profile->last_name }}</td>
+                            </tr>
+                        @endforeach
+                    </tbody>
+                </table>
+            @endif
+        </div>
+    @endif
 </div>
